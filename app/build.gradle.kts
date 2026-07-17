93d0f4dd5e77ff217f7f528c27c9d5a49b8bd184
Miikki10 <bruno.milicevic12@gmail.com>
2026-07-17 19:25:07 +0200

    priprema za produkciju

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.miky.gymko"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
